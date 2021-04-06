15c7b4f9582c3b792cd707b1246f1b47bd8ab59a
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-06 17:40:22 +0100

    maximum reached
    Co-authored-by: Phil Smithies <philsmithies@gmail.com>
    Co-authored-by: Yohann Tisserand <tisserand.yohann@gmail.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,22 @@
 class Oystercard
   attr_accessor :balance
+  attr_reader :max_balance
+  MAX_BALANCE = 90
 
-  def initialize
+  def initialize(max_balance = MAX_BALANCE)
     @balance = 0
+    @max_balance = max_balance
   end
 
   def top_up(amount)
+    raise "The maximum balance of #{max_balance} has been reached" if  @balance + amount > @max_balance
+
     @balance += amount
   end
+
+  private 
+
+  # def max?
+  #   @balance + amount > @max_balance
+  # end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,17 +1,30 @@
 require 'oystercard'
 
 describe Oystercard do 
+
+ 
   
   context '#initialize' do
     it { is_expected.to respond_to(:balance) }
+    it { is_expected.to respond_to(:max_balance) }
 
     it 'has a default value' do
       expect(subject.balance).to eq(0)
     end
+
+     it 'has a maximum balance' do
+       expect(Oystercard::MAX_BALANCE).to eq 90
+    end
+
+    it 'raises an error when max reached' do
+      maximum_balance = Oystercard::MAX_BALANCE
+      subject.top_up(maximum_balance)
+      expect { subject.top_up 1 }.to raise_error "The maximum balance of #{maximum_balance} has been reached"
+    end
   end
 
   context '#topup' do
-    it { is_expected.to respond_to(:top_up).with(1).argument}
+    it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'Can top up the balance' do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
