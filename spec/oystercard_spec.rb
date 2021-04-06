37f7dc517ea06142991e89fb659d6c826031bddf
Phil Smithies <philsmithies@gmail.com>
2021-04-06 17:21:47 +0200

    Updated the card to be able to top up
    Co-authored-by: Yohann Tisserand <tisserand.yohann@gmail.com>
    Co-authored-by: Phil Smithies <philsmithies@gmail.com>

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,19 @@ require 'oystercard'
 
 describe Oystercard do 
   
-  it { is_expected.to respond_to(:balance) }
+  context '#initialize' do
+    it { is_expected.to respond_to(:balance) }
 
-  it 'has a default value' do
-    expect(subject.balance).to eq(0)
+    it 'has a default value' do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
+  context '#topup' do
+    it { is_expected.to respond_to(:top_up).with(1).argument}
+
+    it 'Can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
   end
 end
